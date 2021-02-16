a17ff7cf8e584e7c8204a22eca1afff31f8c565c
khoma <ks.khoma@gmail.com>
2021-02-16 17:51:47 +0300

    done

---
+++ Большая задача по Maps API. Часть №1.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import pygame
+import requests
+
+coord = input().split(',')
+coord = ','.join(coord)
+map_request = f"https://static-maps.yandex.ru/1.x/?ll={coord}&spn=1.0,1.0&l=map"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
